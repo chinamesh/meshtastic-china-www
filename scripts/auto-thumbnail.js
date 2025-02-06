a0e134cda8f466f08434128fc98e8f202683b1a3
BG6TNB <BG6TNB@foxmail.com>
2025-02-06 15:36:47 +0800

    update URL

--- scripts/auto-thumbnail.js
+++ scripts/auto-thumbnail.js
@@ -1,10 +1,10 @@
-const { url, root } = hexo.config;
+const { url } = hexo.config;
 
 hexo.extend.filter.register('before_post_render', function (data) {
     if (data.thumbnail) {
         // 如果 thumbnail 不是以 http 或 https 开头，自动拼接 url 和 root
         if (!/^https?:\/\//.test(data.thumbnail)) {
-            data.thumbnail = url + root + data.thumbnail;
+            data.thumbnail = url + data.thumbnail;
         }
     }
     return data;
